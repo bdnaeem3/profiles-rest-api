2bbc21161e4c02c3d662daf32f7291682560f330
Naeem Ahmed <naeem@haltrip.com>
2019-11-14 12:34:13 +0600

    First APIView added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+#
+# # Create your views here.
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloAPIView(APIView):
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Naeem Ahmed',
+            'Nayon Ahmed',
+            'Sadik',
+        ]
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
